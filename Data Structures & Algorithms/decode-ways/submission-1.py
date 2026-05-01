97b9acb6ec943587960ee1d0103e007415625e1f
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-05-01 10:11:49 -0700

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,36 @@
+# we can take a double digit val only when 1st digit starts with 1 , then 2nd digit
+# can be b/w (0-9) as 10 - 19 < 26
+# start with 2 , then 2nd digit has to be b/w 0 to 6
+# start with 3,4,5 ... then no 2nd digit
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        seen = {}
+        def memoization(i):
+            if i == len(s):
+                return 1
+
+            if i in seen:
+                return seen[i]
+            
+            if s[i] == '0':
+                return 0
+            
+            seen[i] = memoization(i+1)
+            if i + 1 < len(s) and 9 < int(s[i:i+2]) < 27:
+                seen[i] = seen[i] + memoization(i+2)
+
+            return seen[i]
+        
+        return memoization(0)
+        # def dfs(i):
+        #     if i == len(s):
+        #         return 1
+        #     if s[i] == '0':
+        #         return 0
+        #     res = dfs(i+1)
+
+        #     if i + 1 < len(s) and 9 < int(s[i:i+2]) < 27:
+        #         res = res + dfs(i+2)
+        #     return res
+        # return dfs(0)
+
