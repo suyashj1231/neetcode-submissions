44ce750db89e6390a05fa02aa23d37cdcc0ea155
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-05-02 21:59:20 -0700

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadanes algorithm
+        # currSum = 0
+        # maxSum = nums[0]
+
+        # for num in nums:
+        #     currSum = max(0, currSum)
+        #     currSum += num
+        #     maxSum = max(currSum, maxSum)
+
+        # return maxSum
+
+        # sliding Window
+        currSum = 0
+        maxSum = nums[0]
+        L = 0
+        
+        for R in range(len(nums)):
+            if currSum < 0:
+                currSum = 0
+                L = R
+            
+            currSum += nums[R]
+            if currSum > maxSum:
+                maxSum = currSum
+        
+        return maxSum
+
+
+
