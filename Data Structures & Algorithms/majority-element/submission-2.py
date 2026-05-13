e17bd19309118f42580343d0805d490ba4644498
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-05-13 14:26:39 -0700

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        currmax = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == currmax:
+                count += 1
+            
+            elif count <= 0:
+                currmax = nums[i]
+                count = 1
+                
+            else:
+                count -=1
+        return currmax
+            
+            
