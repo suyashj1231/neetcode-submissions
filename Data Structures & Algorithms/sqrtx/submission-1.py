953c20291d1ea85c4c1aeba59447971cf49c51fb
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-07-03 15:14:37 -0700

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        end = x // 2
+        l = 0
+        r = end
+        if x == 1:
+            return 1
+        while l <= r:
+            mid = (l + (r-l) // 2)
+
+            if mid*mid > x:
+                r = mid - 1
+            
+            elif mid*mid < x:
+                l = mid + 1
+                ans = mid
+            
+            else:
+                return mid
+        
+        return ans
