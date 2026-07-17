e0b4f816ca2486794929f01f71c0838ac446cab9
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-07-17 08:19:39 -0700

    Add: first-missing-positive - submission-5

---
+++ Data Structures & Algorithms/first-missing-positive/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        length = len(nums)
+        for i in range(length):
+            if nums[i] < 0:
+                nums[i] = 0
+        
+
+        for i in range(length):
+            val = abs(nums[i])
+
+            if 1 <= val <= length:
+                if nums[val-1] > 0:
+                    nums[val-1] *= -1
+
+                elif nums[val-1] == 0:
+                     nums[val-1] = -1 * (length+1)
+        
+        for i in range(1, length+1):
+            if nums[i-1] >= 0:
+                return i
+        
+        return length + 1
+
+
+
