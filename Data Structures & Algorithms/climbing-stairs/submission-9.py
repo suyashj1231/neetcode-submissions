06f485a70e9aae250ee93994ead34b7ba76e9171
Suyash Jain <149845903+suyashj1231@users.noreply.github.com>
2026-08-08 01:05:20 -0700

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,43 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0] * (n+1) 
+        dp[1] = 1 # ways to reach step 1 is 1 and
+        dp[2] = 2 # ways to reach step 2 is 22...
+        
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+
+
+        # seen = {}
+        # def memoization(i):
+        #     if i in seen:
+        #         return seen[i]
+        #     if i == n:
+        #         return 1
+        #     elif i>n:
+        #         return 0
+        #     seen[i] =  memoization(i+1) + memoization(i+2)
+        #     return seen[i]
+        # return memoization(0)
+
+
+        # ways = []
+        # def dfs(i):
+        #     if i == n:
+        #         ways.append(1)
+        #         return
+
+        #     if i>n:
+        #         return
+
+        #     dfs(i+1)
+        #     dfs(i+2)
+
+        # dfs(0)
+        # return sum(ways)
